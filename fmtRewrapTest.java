5dd8720747214a8616950d94e4bc3a82db5bc516
James Dressel <ubergeek@jamesdressel.com>
2012-02-08 18:51:02 -0500

    DOC: Added TODO's to keep track of test cases that I need to write
    I realize this is not best practices, but this assignment is a bit
    small to break out the github issue tracker.

--- fmtRewrapTest.java
+++ fmtRewrapTest.java
@@ -4,6 +4,18 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+//TODO: happy path
+//TODO: width shorter than word
+//TODO: width same size as word
+//TODO: null string
+//TODO: empty string
+//TODO: line break
+//TODO: white space
+//TODO: zero size
+//TODO: negative size
+//TODO: no spaces
+//TODO: all spaces
+//TODO: non-printable
 
 /**
  * Test file for fmtRewrap
