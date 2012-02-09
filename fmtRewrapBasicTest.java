518d972fa2aab3784f447699780885be2c7dec35
James Dressel <ubergeek@jamesdressel.com>
2012-02-08 19:21:58 -0500

    FEATURE: Catch exception in zeroSizeTest

--- fmtRewrapBasicTest.java
+++ fmtRewrapBasicTest.java
@@ -46,7 +46,13 @@ public class fmtRewrapBasicTest {
 		//to what I have here, but it exposes the inner workings of the class
 		//in a way that it should not. 
 		input = "A test string";
-		System.out.print(fmtRewrap.fmtRewrap("A test string",0));
+		try{
+			System.out.print(fmtRewrap.fmtRewrap(input,0));
+			fail("This code should have caused an IllegalArgumentException");
+		} catch(IllegalArgumentException e){
+			
+		}
+		
 	}
 	
 }
